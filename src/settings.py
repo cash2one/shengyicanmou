0663fac1e72c0ddf77f01235387287291cbe2f0d
unknown <liling1@jk.com>
2017-06-22 20:17:49 +0800

    verify login

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,15 @@
 import os
 
+HEADERS = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+            "Accept-encoding": "gzip",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch, br",
+            "Accept-Language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            }
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 LOGGING = {
--- src/sycm.py
+++ src/sycm.py
@@ -6,28 +6,27 @@ import logging.config
 import time
 import pprint
 import json
+import re
 
 import requests
 from selenium import webdriver
 
-from settings import LOGGING
+from settings import LOGGING, HEADERS
 
 logging.config.dictConfig(LOGGING)
 logger = logging.getLogger('myspider')
 
 class Sycm(object):
     def __init__(self,  username, passwd):
+        self.data_url = 'https://sycm.taobao.com/mq/industry/product/detail.htm?spm=a21ag.7782686.0.0.c8PRca#/?brandId=3228590&cateId=50023717&dateRange=2017-06-21%7C2017-06-21&dateType=recent1&device=0&modelId=277847275&seller=-1&spuId=277847275'
         self.username = username
         self.passwd = passwd
         self.session = requests.Session()
-        self._login(username, passwd)
-        try:
-            self._get_cookies()
-        except IOError as e:
-            logger.debug(e)
+        # self._login(username, passwd)
+
+        self._get_login_cookies()
         if self._check_login():            
-            logger.debug('from cache...cookies...')
-            logger.debug(self.session.cookies)    
+            logger.debug('from cache...cookies...,login success')
             self.crawl_data()
         else:
             # 防止cookie过期失效
@@ -68,15 +67,27 @@ class Sycm(object):
             json.dump(login_cookies, f)
         self.session.cookies.update(login_cookies)
 
-     def _get_cookies(self):
+    def _get_login_cookies(self):
         """从文本中获得cookies
         """
         with open("login_cookies.json") as f:
             cookies = json.load(f)
             self.session.cookies.update(cookies)
 
-    def _update_login_cookies(self):
-        pass
+    def _check_login(self):
+        """验证是否登陆成功
+        Returns:
+            Boolean: 是否登陆成功
+        """
+        res = self.session.get(self.data_url, headers=HEADERS, verify=False)
+        # import pdb
+        # pdb.set_trace()
+        text = res.text
+        login_key = re.findall(r'title="登出"', text)
+        if login_key:
+            return True
+        else:
+            return False
 
     def crawl_data(self):
         pass
