931f0977cceb1756deec5c501bd1791b77f75a5d
unknown <liling1@jk.com>
2017-06-23 19:33:30 +0800

    use phantomjs

--- src/sycm_linux.py
+++ src/sycm_linux.py
@@ -24,26 +24,27 @@ class Sycm(object):
         self.username = username
         self.passwd = passwd
         self.session = requests.Session()
-        # self._login(username, passwd)
+        self._login(username, passwd)
 
-        self._get_login_cookies()
-        if self._check_login():            
-            logger.debug('from cache...cookies...,login success')
-            # self.crawl_industry_data()
-            self.crawl_list_item_trend()
-        else:
-            # 防止cookie过期失效
-            self.session.cookies.clear()
-            self._login(username, passwd)
-            self.crawl_list_item_trend()
+        # self._get_login_cookies()
+        # if self._check_login():            
+        #     logger.debug('from cache...cookies...,login success')
+        #     # self.crawl_industry_data()
+        #     self.crawl_list_item_trend()
+        # else:
+        #     # 防止cookie过期失效
+        #     self.session.cookies.clear()
+        #     self._login(username, passwd)
+        #     self.crawl_list_item_trend()
 
     def _login(self, username, passwd):
         # url = 'https://sycm.taobao.com'
         login_url = 'https://login.taobao.com/member/login.jhtml?from=sycm&full_redirect=true&style=minisimple&minititle=&minipara=0,0,0&sub=true&redirect_url=http://sycm.taobao.com/'
-        driver = webdriver.Chrome(
-            executable_path="E:/Program Files (x86)/chromedriver"
-            # executable_path="C:/Program Files (x86)/Google/Chrome/Application/chromedriver"
-            )
+        # driver = webdriver.Chrome(
+        #     executable_path="E:/Program Files (x86)/chromedriver"
+        #     # executable_path="C:/Program Files (x86)/Google/Chrome/Application/chromedriver"
+        #     )
+        driver = webdriver.PhantomJS()
         driver.maximize_window()
         driver.get(login_url)
     
@@ -146,10 +147,11 @@ class Sycm(object):
         if driver:
             driver.get(product_url)
         else:
-            driver = webdriver.Chrome(
-                executable_path="E:/Program Files (x86)/chromedriver"
-                # executable_path="C:/Program Files (x86)/Google/Chrome/Application/chromedriver"
-            )
+            # driver = webdriver.Chrome(
+            #     executable_path="E:/Program Files (x86)/chromedriver"
+            #     # executable_path="C:/Program Files (x86)/Google/Chrome/Application/chromedriver"
+            # )
+            driver = webdriver.PhantomJS()
             driver.maximize_window()
             driver.get(product_url)
             driver.add_cookie(self._get_login_cookies())    # 增加保存到本地的cookies，实现带cookies登录
@@ -166,7 +168,14 @@ class Sycm(object):
             logger.debug('开始获取第{}页数据'.format(page)+'-'*20)
             self.analysis_industry_html(page_source)
           
-    
+    def get_list_item_total_page(self):
+        list_items_url = 'https://sycm.taobao.com/mq/rank/listItems.json?cateId=50023717&categoryId=50023717&dateRange=2017-06-22%7C2017-06-22&dateRangePre=2017-06-22|2017-06-22&dateType=recent1&dateTypePre=recent1&device=0&devicePre=0&itemDetailType=1&keyword=&orderDirection=desc&orderField=payOrdCnt&page=1&pageSize=100&rankTabIndex=0&rankType=1&seller=-1&token=aa970f317&view=rank&_=1498206609142'
+        res = self.session.get(url=list_items_url, headers=HEADERS, verify=False)
+        list_items = json.loads(res.text)
+        total_items_count = list_items['content']['data']['recordCount']
+        total_page = int(total_items_count/100)
+        return total_page
+
     def crawl_list_item_trend(self):
         '''
         抓取 市场->市场店铺榜->查看详情->商品趋势 数据
@@ -177,26 +186,29 @@ class Sycm(object):
         # import pdb
         # pdb.set_trace()
         # 获得 商品信息(itemTitle)、商品id(itemId)
-        list_items_url = 'https://sycm.taobao.com/mq/rank/listItems.json?cateId=50023717&categoryId=50023717&dateRange=2017-06-22%7C2017-06-22&dateRangePre=2017-06-22|2017-06-22&dateType=recent1&dateTypePre=recent1&device=0&devicePre=0&itemDetailType=1&keyword=&orderDirection=desc&orderField=payOrdCnt&page=1&pageSize=100&rankTabIndex=0&rankType=1&seller=-1&token=aa970f317&view=rank&_=1498206609142'
-        res = self.session.get(url=list_items_url, headers=HEADERS, verify=False)
-        list_items = json.loads(res.text)
-        total_items_count = list_items['content']['data']['recordCount']
-        total_page = int(total_items_count/100)
-        total_items_data = list_items['content']['data']['data']
-        for item in total_items_data:
-            #item keys: ['payByrRateIndex', 'mallItem', 'itemUrl', 'itemPicUrl', 'sameGoodUrl', 'itemPrice', 'payOrdCnt', 'itemId', 'shopUrl', 'pvIndex', 'shopName', 'tradeIndexCrc', 'searchIndex', 'orderNum', 'itemTitle']
-            item_title = item['itemTitle']
-            item_id = item['itemId']
-            # 构造 商品趋势的折线图 的URL
-            item_tred_url = 'https://sycm.taobao.com/mq/rank/listItemTrend.json?cateId=50023717&categoryId=50023717&dateRange=2017-06-22%7C2017-06-22&dateRangePre=2017-06-22|2017-06-22&dateType=recent1&dateTypePre=recent1&device=0&devicePre=0&indexes=payOrdCnt,payByrRateIndex,payItemQty&itemDetailType=1&itemId={item_id}&latitude=undefined&rankTabIndex=0&rankType=1&seller=-1&token=aa970f317&view=detail'\
-                            .format(item_id=item_id)
-            tred_res = self.session.get(url=item_tred_url, headers=HEADERS, verify=False)
-            tred_items = json.loads(tred_res.text)
-            pay_item_qtylist = tred_items['content']['data']['payItemQtyList']
-            pay_ord_cntlist = tred_items['content']['data']['payOrdCntList']
-            pay_byr_rate_index_list = tred_items['content']['data']['payByrRateIndexList']
-            logger.debug('所有终端-支付子订单数:{}, 所有终端-支付转化率指数:{}, 所有终端-支付件数:{}'
-                        .format(pay_ord_cntlist, pay_byr_rate_index_list, pay_item_qtylist))
+        total_page = self.get_list_item_total_page()+1
+        for page in range(1, total_page):
+            logger.debug('开始获取第{}页数据'.format(page)+'-'*20)
+            list_items_url = 'https://sycm.taobao.com/mq/rank/listItems.json?cateId=50023717&categoryId=50023717&dateRange=2017-06-22%7C2017-06-22&dateRangePre=2017-06-22|2017-06-22&dateType=recent1&dateTypePre=recent1&device=0&devicePre=0&itemDetailType=1&keyword=&orderDirection=desc&orderField=payOrdCnt&page={page}&pageSize=100&rankTabIndex=0&rankType=1&seller=-1&token=aa970f317&view=rank&_=1498206609142'\
+                            .format(page=page)
+            res = self.session.get(url=list_items_url, headers=HEADERS, verify=False)
+            list_items = json.loads(res.text)
+            total_items_count = list_items['content']['data']['recordCount']
+            total_items_data = list_items['content']['data']['data']
+            for item in total_items_data:
+                #item keys: ['payByrRateIndex', 'mallItem', 'itemUrl', 'itemPicUrl', 'sameGoodUrl', 'itemPrice', 'payOrdCnt', 'itemId', 'shopUrl', 'pvIndex', 'shopName', 'tradeIndexCrc', 'searchIndex', 'orderNum', 'itemTitle']
+                item_title = item['itemTitle']
+                item_id = item['itemId']
+                # 构造 商品趋势的折线图 的URL
+                item_tred_url = 'https://sycm.taobao.com/mq/rank/listItemTrend.json?cateId=50023717&categoryId=50023717&dateRange=2017-06-22%7C2017-06-22&dateRangePre=2017-06-22|2017-06-22&dateType=recent1&dateTypePre=recent1&device=0&devicePre=0&indexes=payOrdCnt,payByrRateIndex,payItemQty&itemDetailType=1&itemId={item_id}&latitude=undefined&rankTabIndex=0&rankType=1&seller=-1&token=aa970f317&view=detail'\
+                                .format(item_id=item_id)
+                tred_res = self.session.get(url=item_tred_url, headers=HEADERS, verify=False)
+                tred_items = json.loads(tred_res.text)
+                pay_item_qtylist = tred_items['content']['data']['payItemQtyList']
+                pay_ord_cntlist = tred_items['content']['data']['payOrdCntList']
+                pay_byr_rate_index_list = tred_items['content']['data']['payByrRateIndexList']
+                logger.debug('产品：{}, 所有终端-支付子订单数:{}, 所有终端-支付转化率指数:{}, 所有终端-支付件数:{}'
+                            .format(item_title, pay_ord_cntlist, pay_byr_rate_index_list, pay_item_qtylist))
            
 
 if __name__ == '__main__':
